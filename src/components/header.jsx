f78b93af6a5131abb8bc1bf75be1a2710eb0e286
jwang0110 <johnw7@uci.edu>
2021-07-15 12:02:57 -0700

    set up responsive header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import "../styles/header.scss";
 
 const Header = () => {
 	return (
-		<header>
-			<nav>
-				<img
-					src="https://global-uploads.webflow.com/5deea2e254c6bb904cf59c96/5f887df85a2b176d90ddf64e_headerlogo.svg"
-					alt=""
-				/>
-				<ul>
-					<li>Home</li>
-					<li>About Us</li>
-					<li>Try It</li>
-				</ul>
-			</nav>
+		<header className="header">
+			<div className="header__content">
+				<div className="logo">
+					<img
+						src="https://global-uploads.webflow.com/5deea2e254c6bb904cf59c96/5f887df85a2b176d90ddf64e_headerlogo.svg"
+						alt=""
+					/>
+				</div>
+				<nav className="navbar">
+					<ul className="navbar__list">
+						<li className="navbar__item">
+							<a className="navbar__link" href="#">
+								Home
+							</a>
+						</li>
+						<li className="navbar__item">
+							<a className="navbar__link" href="#">
+								About Us
+							</a>
+						</li>
+						<li className="navbar__item">
+							<a className="navbar__link" href="#">
+								Try It
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import "../styles/layout.scss";
 
 const Layout = ({ children }) => {
 	return (
-		<div>
-			<div>
-				<Header />
-				<main>{children}</main>
+		<>
+			<div className="layout">
+				<div className="layout__content">
+					<Header />
+					<main>{children}</main>
+				</div>
+				<Footer />
 			</div>
-			<Footer />
-		</div>
+		</>
 	);
 };
 
